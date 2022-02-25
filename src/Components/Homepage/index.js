f47947b96edcd6d087b28dd2c45a8c3b80934dac
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2022-02-25 17:15:43 -0300

    added header to homepage

--- src/Components/Homepage/index.js
+++ src/Components/Homepage/index.js
@@ -1,9 +1,23 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import styles from './homepage.module.css';
 
 function Homepage() {
   return (
-    <div>Homepage</div>
+    <div className={styles.background}>
+      <header>
+        <div className={styles.headerContent}>
+          <div>Company logo</div>
+          <div className={styles.navLinks}>
+            <div>WHO WE ARE</div>
+            <div>NFT</div>
+            <div>CONTACT</div>
+            <div>LOGIN</div>
+          </div>
+        </div>
+      </header>
+      aaaaa
+    </div>
   );
 }
 
