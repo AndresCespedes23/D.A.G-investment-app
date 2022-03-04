b3d3a74c3e7e278caa8fb667b70099afef3bd77f
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2022-03-04 18:15:07 -0300

    basic HTML of nft information

--- src/Components/Homepage/NftInformation/index.js
+++ src/Components/Homepage/NftInformation/index.js
@@ -1,9 +1,19 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import styles from './nft-info.module.css';
 
 function NftInformation() {
   return (
-    <div>NftInformation</div>
+    <section>
+      <div className={styles.title}>
+        <h2>NFTs are a bla bla bla</h2>
+      </div>
+      <div className={styles.nftBtn}>
+        <button type="button">
+          See more
+        </button>
+      </div>
+    </section>
   );
 }
 
