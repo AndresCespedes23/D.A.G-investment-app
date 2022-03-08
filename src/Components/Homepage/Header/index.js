ba961f90af33fcfb2c0066c99df697f1b0b75d27
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2022-03-08 16:47:06 -0300

    added animation to the header

--- src/Components/Homepage/Header/index.js
+++ src/Components/Homepage/Header/index.js
@@ -1,22 +1,33 @@
+/* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
+import { useState } from 'react';
 import styles from './header.module.css';
 import Button from '../../Shared/Button';
 
 function Header() {
+  const [color, setColor] = useState(false);
+  const changeColor = () => {
+    if (window.scrollY >= 90) {
+      setColor(true);
+    } else {
+      setColor(false);
+    }
+  };
+  window.addEventListener('scroll', changeColor);
+
   return (
-    <header className={styles.headerContent}>
-      <div>
+    <header className={color ? styles.headerChangeColor : styles.headerPosition}>
+      <nav className={styles.headerContent}>
         <img className={styles.logo} src={`${process.env.PUBLIC_URL}/assets/img/bennu.png`} alt="bennu logo" />
-      </div>
-      <Button type="bars" />
-      <nav className={styles.navLinks}>
-        <ul>
-          <li>Who we are</li>
-          <li>NFT</li>
-          <li>Contact</li>
-          <li>Login</li>
-        </ul>
+        <Button type="bars" />
+        <div className={styles.navLinks}>
+          <ul>
+            <li>Who we are</li>
+            <li>NFT</li>
+            <li>Contact</li>
+            <li>Login</li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
