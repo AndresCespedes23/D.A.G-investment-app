8db338b1677b8b9917173d8634569e9a656654e6
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2022-03-11 16:24:39 -0300

    changed minor issues in code

--- src/Components/Homepage/ResponsiveNavbar/index.js
+++ src/Components/Homepage/ResponsiveNavbar/index.js
@@ -1,12 +1,14 @@
+/* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
 import styles from './responsive-navbar.module.css';
 import Button from '../../Shared/Button';
 
 function ResponsiveNavbar() {
   return (
     <div className={styles.navbarContainer}>
-      <Button type="close" />
+      <Button
+        type="close"
+      />
       <ul className={styles.navLinks}>
         <li>Who we are</li>
         <li>NFT</li>
