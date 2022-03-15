2b3f56bdff3524b4f0d591d32c9935e1a1950e11
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2022-03-15 13:18:14 -0300

    fetch data from nft's crypto punk

--- src/Components/Homepage/NftBanner/index.js
+++ src/Components/Homepage/NftBanner/index.js
@@ -1,8 +1,22 @@
+/* eslint-disable no-console */
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { Search10CryptoPunks } from './moralis-api-endpoints/api';
 import styles from './banner.module.css';
 
 function NftBanner() {
+  const fetchCryptoPunks = async () => {
+    await axios.get(Search10CryptoPunks(), {
+      headers: {
+        'X-API-KEY': 'xuhY5bsVXeQNxrKvcL2uLMuxqPNChINgBZdXD1z2VuXnpOeWglWIW3k4jgpZiAsn',
+      },
+    });
+  };
+  useEffect(() => {
+    fetchCryptoPunks();
+  }, []);
+
   return (
     <section className={styles.bannerContainer}>NftBanner</section>
   );
---
+++ src/Components/Homepage/NftBanner/moralis-api-endpoints/api.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export const Search10CryptoPunks = () => 'https://deep-index.moralis.io/api/v2/nft/search?chain=eth&format=decimal&q=crypto%20punk&filter=name&limit=10';
