7def8177c871594537d48a7a9e9995e7d07c6dae
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2022-03-16 15:38:40 -0300

    deleted unnecesary components

--- src/Components/Homepage/NftBanner/index.js
+++ src/Components/Homepage/NftBanner/index.js
@@ -1,43 +1,13 @@
 /* eslint-disable arrow-body-style */
 /* eslint-disable no-console */
 /* eslint-disable react/jsx-filename-extension */
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Search10CryptoPunks } from './moralis-api-endpoints/api';
+import React from 'react';
 import styles from './banner.module.css';
 
 function NftBanner() {
-  const [collection, setCollection] = useState([]);
-
-  const fetchCryptoPunks = async () => {
-    await axios.get(Search10CryptoPunks(), {
-      headers: {
-        'X-API-KEY': 'xuhY5bsVXeQNxrKvcL2uLMuxqPNChINgBZdXD1z2VuXnpOeWglWIW3k4jgpZiAsn',
-      },
-    }).then((res) => {
-      console.log(res.data.result);
-      setCollection(res.data.result);
-    }).catch((error) => {
-      console.error(error);
-    });
-  };
-
-  useEffect(() => {
-    fetchCryptoPunks();
-  }, []);
-
   return (
     <section className={styles.bannerContainer}>
-      {collection.length > 0 && (
-        <div>
-          {collection.map((nft) => (
-            <div>
-              <img src={nft?.metadata} alt={nft.name} />
-              <div>{nft.metadata}</div>
-            </div>
-          ))}
-        </div>
-      )}
+      NFT BANNER
     </section>
   );
 }
--- src/Components/Homepage/NftBanner/moralis-api-endpoints/api.js
+++
@@ -1,2 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export const Search10CryptoPunks = () => 'https://deep-index.moralis.io/api/v2/nft/0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb?chain=eth&format=decimal&limit=10';
