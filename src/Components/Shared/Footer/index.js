15e7c67f997006d199ce938bd7ffafb046d33677
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2022-03-21 03:30:52 -0300

    added nav link to home

--- src/Components/Shared/Footer/index.js
+++ src/Components/Shared/Footer/index.js
@@ -1,11 +1,14 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './footer.module.css';
 
 function Footer() {
   return (
     <div className={styles.footerContainer}>
-      <img className={styles.logo} src={`${process.env.PUBLIC_URL}bennu-icon.png`} alt="bennu logo" />
+      <Link to="/">
+        <img className={styles.logo} src={`${process.env.PUBLIC_URL}bennu-icon.png`} alt="bennu logo" />
+      </Link>
       <p className={styles.copyright}>
         ©2022 Bennu
       </p>
