7adb9148c845e5531114c2454fac6e34874b472e
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2022-03-20 21:30:25 -0300

    added neon lights style to nft information button

--- src/Components/Homepage/NftInformation/index.js
+++ src/Components/Homepage/NftInformation/index.js
@@ -7,20 +7,18 @@ function NftInformation() {
   return (
     <section className={styles.nftContainer}>
       <div className={styles.nftShadowEffect}>
-        <div>
-          <h2 className={styles.title}>What are NFTs?</h2>
-          <p className={styles.infoNft}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Etiam sit amet venenatis felis. Aenean sed dapibus mauris.
-            Vestibulum eget blandit risus, sit amet efficitur felis.
-            Suspendisse feugiat metus in tortor aliquam, at tempor enim sollicitudin.
-            Proin luctus ullamcorper nisl a consequat.
-            Proin posuere nisi enim, sed maximus lorem elementum a.
-            elit orci, maximus rhoncus vehicula vitae, vulputate et neque.
-            Etiam venenatis odio vitae justo tristique, sed vehicula lectus varius.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac libero nisi.
-          </p>
-        </div>
+        <h2 className={styles.title}>What are NFTs?</h2>
+        <p className={styles.infoNft}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Etiam sit amet venenatis felis. Aenean sed dapibus mauris.
+          Vestibulum eget blandit risus, sit amet efficitur felis.
+          Suspendisse feugiat metus in tortor aliquam, at tempor enim sollicitudin.
+          Proin luctus ullamcorper nisl a consequat.
+          Proin posuere nisi enim, sed maximus lorem elementum a.
+          elit orci, maximus rhoncus vehicula vitae, vulputate et neque.
+          Etiam venenatis odio vitae justo tristique, sed vehicula lectus varius.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac libero nisi.
+        </p>
         <div className={styles.nftBtn}>
           <Link to="/nft-information">
             <button className={styles.moreInfoBtn} type="button">
