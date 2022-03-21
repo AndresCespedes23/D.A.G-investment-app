26c7537bd3ea3c0dd2605ab41df01bd52513b54f
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2022-03-20 23:54:36 -0300

    added min-height to the component

---
+++ src/Components/NftEducation/Fundamentals/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import styles from './fundamentals.module.css';
+
+function Fundamentals() {
+  return (
+    <div className={styles.title}>Fundamentals</div>
+  );
+}
+
+export default Fundamentals;
--- src/Components/NftEducation/index.js
+++ src/Components/NftEducation/index.js
@@ -1,11 +1,15 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import Header from './Header';
+import Fundamentals from './Fundamentals';
+import Footer from '../Shared/Footer';
 
 function NftEducation() {
   return (
     <div>
       <Header />
+      <Fundamentals />
+      <Footer />
     </div>
   );
 }
