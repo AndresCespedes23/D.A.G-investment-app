54ae653136db38575bcc5ac2f081e14d982aa13c
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2022-03-21 02:12:10 -0300

    added new component to nft education & added hover styles to header links

---
+++ src/Components/NftEducation/Security/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import styles from './security.module.css';
+
+function Security() {
+  return (
+    <div className={styles.securityContainer}>Security</div>
+  );
+}
+
+export default Security;
--- src/Components/NftEducation/index.js
+++ src/Components/NftEducation/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Header from './Header';
 import Fundamentals from './Fundamentals';
+import Security from './Security';
 import Footer from '../Shared/Footer';
 
 function NftEducation() {
@@ -9,6 +10,7 @@ function NftEducation() {
     <div>
       <Header />
       <Fundamentals />
+      <Security />
       <Footer />
     </div>
   );
