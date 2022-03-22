18da2e8b14d715912ba65c0284c39e89a83bd440
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2022-03-22 14:12:43 -0300

    changed font style of cards

--- src/Components/Homepage/NftInformation/index.js
+++ src/Components/Homepage/NftInformation/index.js
@@ -8,36 +8,40 @@ function NftInformation() {
     <section className={styles.nftContainer} id="nft">
       <div className={styles.nftShadowEffect}>
         <h2 className={styles.title}>What are NFTs?</h2>
-        <Link to="/nft-education/fundamentals">
-          <div className={styles.educationCard}>
+        <div className={styles.educationCard}>
+          <Link to="/nft-education/fundamentals">
             <img className={styles.logo} src={`${process.env.PUBLIC_URL}/assets/img/unicornio.png`} alt="fundamentals" />
-            <h2>Fundamentals</h2>
-          </div>
-        </Link>
-        <Link to="/nft-education/ownership">
-          <div className={styles.educationCard}>
+          </Link>
+          <h2 className={styles.cardTitle}>Fundamentals</h2>
+        </div>
+        <div className={styles.educationCard}>
+          <Link to="/nft-education/ownership">
             <img className={styles.logo} src={`${process.env.PUBLIC_URL}/assets/img/llave.png`} alt="ownership" />
-            <h2>Ownership</h2>
-          </div>
-        </Link>
-        <Link to="/nft-education/minting">
-          <div className={styles.educationCard}>
+          </Link>
+          <h2 className={styles.cardTitle}>Ownership</h2>
+        </div>
+        <div className={styles.educationCard}>
+          <Link to="/nft-education/minting">
             <img className={styles.logo} src={`${process.env.PUBLIC_URL}/assets/img/varita-magica.png`} alt="varita magica" />
-            <h2>Minting</h2>
-          </div>
-        </Link>
-        <Link to="/nft-education/security">
-          <div className={styles.educationCard}>
+          </Link>
+          <h2 className={styles.cardTitle}>Minting</h2>
+        </div>
+        <div className={styles.educationCard}>
+          <Link to="/nft-education/security">
             <img className={styles.logo} src={`${process.env.PUBLIC_URL}/assets/img/proteger.png`} alt="security" />
-            <h2>Security</h2>
-          </div>
-        </Link>
-        <Link to="/nft-education/use-cases">
-          <div className={styles.educationCard}>
-            <img className={styles.logo} src={`${process.env.PUBLIC_URL}/assets/img/bombilla.png`} alt="nft use cases" />
-            <h2>NFTs use cases</h2>
-          </div>
-        </Link>
+          </Link>
+          <h2 className={styles.cardTitle}>Security</h2>
+        </div>
+        <div className={styles.educationCard}>
+          <Link to="/nft-education/use-cases">
+            <img
+              className={styles.logo}
+              src={`${process.env.PUBLIC_URL}/assets/img/bombilla.png`}
+              alt="nft use cases"
+            />
+          </Link>
+          <h2 className={styles.cardTitle}>NFTs use cases</h2>
+        </div>
       </div>
     </section>
   );
