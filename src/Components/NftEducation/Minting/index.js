219fd1945a511fd033da86d60341ad52a4cfb59f
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2022-03-22 11:20:49 -0300

    deleted unnecesary carousel

--- src/Components/NftEducation/Minting/index.js
+++ src/Components/NftEducation/Minting/index.js
@@ -3,11 +3,13 @@ import React from 'react';
 import styles from './minting.module.css';
 
 function Minting() {
+
   return (
     <div className={styles.mintingContainer}>
-      <div>Minting</div>
-      <img className={styles.logo} src={`${process.env.PUBLIC_URL}/assets/img/varita-magica.png`} alt="varita magica" />
+        <img className={styles.logo} src={`${process.env.PUBLIC_URL}/assets/img/varita-magica.png`} alt="varita magica" />
+        <div>Minting</div>
     </div>
+
   );
 }
 
