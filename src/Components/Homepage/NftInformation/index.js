cb91b9201b05c8d26421983de5f02b648369a64a
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2022-03-22 13:44:31 -0300

    added route path to the buttons in homepage

--- src/Components/Homepage/NftInformation/index.js
+++ src/Components/Homepage/NftInformation/index.js
@@ -8,28 +8,36 @@ function NftInformation() {
     <section className={styles.nftContainer} id="nft">
       <div className={styles.nftShadowEffect}>
         <h2 className={styles.title}>What are NFTs?</h2>
-        <Link to="/nft-information">
+        <Link to="/nft-education/fundamentals">
           <div className={styles.educationCard}>
             <img className={styles.logo} src={`${process.env.PUBLIC_URL}/assets/img/unicornio.png`} alt="fundamentals" />
             <h2>Fundamentals</h2>
           </div>
         </Link>
-        <div className={styles.educationCard}>
-          <img className={styles.logo} src={`${process.env.PUBLIC_URL}/assets/img/llave.png`} alt="ownership" />
-          <h2>Ownership</h2>
-        </div>
-        <div className={styles.educationCard}>
-          <img className={styles.logo} src={`${process.env.PUBLIC_URL}/assets/img/varita-magica.png`} alt="varita magica" />
-          <h2>Minting</h2>
-        </div>
-        <div className={styles.educationCard}>
-          <img className={styles.logo} src={`${process.env.PUBLIC_URL}/assets/img/proteger.png`} alt="security" />
-          <h2>Security</h2>
-        </div>
-        <div className={styles.educationCard}>
-          <img className={styles.logo} src={`${process.env.PUBLIC_URL}/assets/img/bombilla.png`} alt="nft use cases" />
-          <h2>NFTs use cases</h2>
-        </div>
+        <Link to="/nft-education/ownership">
+          <div className={styles.educationCard}>
+            <img className={styles.logo} src={`${process.env.PUBLIC_URL}/assets/img/llave.png`} alt="ownership" />
+            <h2>Ownership</h2>
+          </div>
+        </Link>
+        <Link to="/nft-education/minting">
+          <div className={styles.educationCard}>
+            <img className={styles.logo} src={`${process.env.PUBLIC_URL}/assets/img/varita-magica.png`} alt="varita magica" />
+            <h2>Minting</h2>
+          </div>
+        </Link>
+        <Link to="/nft-education/security">
+          <div className={styles.educationCard}>
+            <img className={styles.logo} src={`${process.env.PUBLIC_URL}/assets/img/proteger.png`} alt="security" />
+            <h2>Security</h2>
+          </div>
+        </Link>
+        <Link to="/nft-education/use-cases">
+          <div className={styles.educationCard}>
+            <img className={styles.logo} src={`${process.env.PUBLIC_URL}/assets/img/bombilla.png`} alt="nft use cases" />
+            <h2>NFTs use cases</h2>
+          </div>
+        </Link>
       </div>
     </section>
   );
---
+++ src/Components/NftEducation/Fundamentals/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+
+function Fundamentals() {
+  return (
+    <div>Fundamentals</div>
+  );
+}
+
+export default Fundamentals;
