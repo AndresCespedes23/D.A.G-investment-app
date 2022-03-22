24024578595e9494958e499848cb4a3302553c61
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2022-03-22 17:12:01 -0300

    added animation to cards & changed css of header

--- src/Components/Homepage/NftInformation/index.js
+++ src/Components/Homepage/NftInformation/index.js
@@ -1,38 +1,43 @@
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 import styles from './nft-info.module.css';
 
 function NftInformation() {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
   return (
     <section className={styles.nftContainer} id="nft">
       <div className={styles.nftShadowEffect}>
         <h2 className={styles.title}>What are NFTs?</h2>
-        <div className={styles.educationCard}>
+        <div className={styles.educationCard} data-aos="fade-up">
           <Link to="/nft-education/fundamentals">
             <img className={styles.logo} src={`${process.env.PUBLIC_URL}/assets/img/unicornio.png`} alt="fundamentals" />
           </Link>
           <h2 className={styles.cardTitle}>Fundamentals</h2>
         </div>
-        <div className={styles.educationCard}>
+        <div className={styles.educationCard} data-aos="fade-up">
           <Link to="/nft-education/ownership">
             <img className={styles.logo} src={`${process.env.PUBLIC_URL}/assets/img/llave.png`} alt="ownership" />
           </Link>
           <h2 className={styles.cardTitle}>Ownership</h2>
         </div>
-        <div className={styles.educationCard}>
+        <div className={styles.educationCard} data-aos="fade-up">
           <Link to="/nft-education/minting">
             <img className={styles.logo} src={`${process.env.PUBLIC_URL}/assets/img/varita-magica.png`} alt="varita magica" />
           </Link>
           <h2 className={styles.cardTitle}>Minting</h2>
         </div>
-        <div className={styles.educationCard}>
+        <div className={styles.educationCard} data-aos="fade-up">
           <Link to="/nft-education/security">
             <img className={styles.logo} src={`${process.env.PUBLIC_URL}/assets/img/proteger.png`} alt="security" />
           </Link>
           <h2 className={styles.cardTitle}>Security</h2>
         </div>
-        <div className={styles.educationCard}>
+        <div className={styles.educationCard} data-aos="fade-up">
           <Link to="/nft-education/use-cases">
             <img
               className={styles.logo}
