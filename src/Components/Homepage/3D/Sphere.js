3a85f986e5f082374f48a96d0265d588697f06ec
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2022-03-31 15:01:09 -0300

    added animations and light to the models

--- src/Components/Homepage/3D/Sphere.js
+++ src/Components/Homepage/3D/Sphere.js
@@ -1,11 +1,51 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import { Instances, Instance } from '@react-three/drei';
+import { useFrame } from 'react-three-fiber';
 
 function Sphere() {
+  const myMesh = React.useRef();
+
+  useFrame(() => {
+    myMesh.current.rotation.x += 0.075;
+  });
+
   return (
-    <mesh>
-      <sphereGeometry />
-      <meshStandardMaterial color="#d6b400" wireframe />
+    <mesh ref={myMesh}>
+      <Instances>
+        <icosahedronGeometry />
+        <meshStandardMaterial />
+        <Instance
+          color="yellow"
+          scale={0.1}
+          position={[3, 0, 1]}
+          rotation={[Math.PI / 3, 0, 0]}
+        />
+        <Instance
+          color="yellow"
+          scale={0.2}
+          position={[-3, 0, 1]}
+          rotation={[Math.PI / 3, 0, 0]}
+        />
+        <Instance
+          color="yellow"
+          scale={0.3}
+          position={[-3, 0, -1]}
+          rotation={[Math.PI / 3, 0, 0]}
+        />
+        <Instance
+          color="yellow"
+          scale={0.4}
+          position={[-3, 1, 1]}
+          rotation={[Math.PI / 3, 0, 0]}
+        />
+        <Instance
+          color="yellow"
+          scale={0.5}
+          position={[-3, -1, 1]}
+          rotation={[Math.PI / 3, 0, 0]}
+        />
+      </Instances>
     </mesh>
   );
 }
--- src/Components/Homepage/index.js
+++ src/Components/Homepage/index.js
@@ -1,10 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/self-closing-comp */
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
-import { Stars } from '@react-three/drei';
-import ThreeScene from './3D/ThreeScene';
+import React, { Suspense } from 'react';
+import {
+  Stars, Text,
+} from '@react-three/drei';
 import Sphere from './3D/Sphere';
+import ThreeScene from './3D/ThreeScene';
 import Header from './Header';
 import WhoWeAre from './WhoWeAre';
 import NftInformation from './NftInformation';
@@ -15,18 +17,27 @@ function Homepage() {
   return (
     <>
       <Header />
-      <div style={{ height: '100vh', overflow: 'hidden' }}>
+      <div>
         <ThreeScene>
-          <color attach="background" args={['#090909']} />
+          <Suspense fallback={null}>
+            <Text color="#d6b400" fontSize="1">
+              Bennu
+            </Text>
+          </Suspense>
+
           <Sphere />
+
+          <color attach="background" args={['#090909']} />
           <ambientLight />
+          <pointLight position={[5, 5, 5]} />
           <Stars depth={200} />
         </ThreeScene>
+        <WhoWeAre />
+        <NftInformation />
+        <Contact />
+        <Footer />
       </div>
-      <WhoWeAre />
-      <NftInformation />
-      <Contact />
-      <Footer />
+
     </>
 
   );
